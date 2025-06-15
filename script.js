bb9182e5352773c966578f7f1a1712b91ef63cc6
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-06-15 21:06:23 +0100

    update

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskInput  = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
+    const addTask = () => {
+
+    };
+
 });
